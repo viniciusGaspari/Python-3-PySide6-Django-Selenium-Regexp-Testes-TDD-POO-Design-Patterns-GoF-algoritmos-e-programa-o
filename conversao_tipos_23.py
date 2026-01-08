2331ab682cc5b6db4cfd2be49af2d554ebd1d10d
Vinícius <128446286+viniciusGaspari@users.noreply.github.com>
2026-01-08 14:17:14 -0300

    Delete conversao_tipos_23.py

--- conversao_tipos_23.py
+++
@@ -1,9 +0,0 @@
-# conversão de tipos, coerção
-# print('1' + 1) erro
-print(1 + 1)
-print('a' + 'b')
-print(int('1') + 1, type(int('1')))
-print(float(1) + 1.2)
-print(bool('0'))
-print(bool(''))
-print(str(11) + ' números convertidos')
